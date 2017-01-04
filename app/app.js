9a14ef8a7351269e873076bad3fa8358b9ff57b7
elklein96 <elklein96@gmail.com>
2017-01-03 22:50:47 -0500

    working electron

---
+++ app/app.js
@@ -0,0 +1,4 @@
+(function () {
+    'use strict';
+    angular.module('SmsSync', ['firebase', 'SmsSync.core'])
+})();
--- public/app/app.js
+++
@@ -1,4 +0,0 @@
-(function () {
-    'use strict';
-    angular.module('SmsSync', ['firebase', 'SmsSync.core']);
-})();
